5f9014d2f16846f24b51c1bf28739708a13070bc
nzlateva <72468080+nzlateva@users.noreply.github.com>
2021-10-02 11:36:55 +0300

    Worked on TODO - map OfferEntity to OfferSummaryView

--- mobilelele/src/main/java/bg/softuni/mobilelele/config/ApplicationConfig.java
+++ mobilelele/src/main/java/bg/softuni/mobilelele/config/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package bg.softuni.mobilelele.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -8,9 +9,14 @@ import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 @Configuration
 public class ApplicationConfig {
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new Pbkdf2PasswordEncoder();
-  }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Pbkdf2PasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
 }
