79b00c5ddcd6f21c0a1dd0be343916733a66dc95
luchob <lachezar.balev@gmail.com>
2021-10-05 21:58:12 +0300

    Path var in long.

--- mobilelele/src/main/java/bg/softuni/mobilelele/web/OffersController.java
+++ mobilelele/src/main/java/bg/softuni/mobilelele/web/OffersController.java
@@ -4,6 +4,7 @@ import bg.softuni.mobilelele.service.OfferService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class OffersController {
@@ -19,6 +20,11 @@ public class OffersController {
     model.addAttribute("offers",
         offerService.getAllOffers());
     return "offers";
+  }
+
+  @GetMapping("/offers/{id}/details")
+  public String showOffer(@PathVariable Long id) {
+    return "details";
   }
 
 }
